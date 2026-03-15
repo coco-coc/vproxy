1a3f3a1d9a48fdae607330d022cb470478617e9d
x <5vnetwork@proton.me>
2026-03-16 05:30:37 +0800

    build number 2048

--- lib/main.dart
+++ lib/main.dart
@@ -486,27 +486,29 @@ void main() async {
   );
 
   if (isProduction()) {
-    if (Platform.isWindows || Platform.isLinux) {
-      await SentryFlutter.init((options) {
-        options.dsn =
-            'https://009ab463981b5d206a8d733ebd4182ec@o4510866353618944.ingest.us.sentry.io/4510866355257344';
-        // Adds request headers and IP for users, for more info visit:
-        // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
-        options.sendDefaultPii = false;
-        // if (!isProduction()) {
-        //   options.enableLogs = true;
-        //   // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-        //   // We recommend adjusting this value in production.
-        //   options.tracesSampleRate = 1.0;
-        //   // The sampling rate for profiling is relative to tracesSampleRate
-        //   // Setting to 1.0 will profile 100% of sampled transactions:
-        //   options.profilesSampleRate = 1.0;
-        //   // Configure Session Replay
-        //   options.replay.sessionSampleRate = 0.1;
-        //   options.replay.onErrorSampleRate = 1.0;
-        // }
-      }, appRunner: () => runApp(SentryWidget(child: app)));
-    } else {}
+    // if (Platform.isWindows || Platform.isLinux) {
+    await SentryFlutter.init((options) {
+      options.dsn =
+          'https://009ab463981b5d206a8d733ebd4182ec@o4510866353618944.ingest.us.sentry.io/4510866355257344';
+      // Adds request headers and IP for users, for more info visit:
+      // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
+      options.sendDefaultPii = false;
+      // if (!isProduction()) {
+      //   options.enableLogs = true;
+      //   // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      //   // We recommend adjusting this value in production.
+      //   options.tracesSampleRate = 1.0;
+      //   // The sampling rate for profiling is relative to tracesSampleRate
+      //   // Setting to 1.0 will profile 100% of sampled transactions:
+      //   options.profilesSampleRate = 1.0;
+      //   // Configure Session Replay
+      //   options.replay.sessionSampleRate = 0.1;
+      //   options.replay.onErrorSampleRate = 1.0;
+      // }
+    }, appRunner: () => runApp(SentryWidget(child: app)));
+    // } else {
+    //   runApp(app);
+    // }
   } else {
     runApp(app);
   }
